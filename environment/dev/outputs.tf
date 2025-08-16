1b5ca00fd6312323ee7b7d9cfe0c2ac40510001d
Jagadishhs69 <hsjagadish42@gmail.com>
2025-08-16 22:40:51 +0530

    updated secrets

--- environment/dev/outputs.tf
+++ environment/dev/outputs.tf
@@ -9,3 +9,13 @@ output "dev_instance_public_ip" {
 output "dev_rds_endpoint" {
   value = module.rds.rds_endpoint
 }
+
+output "db_secret_name" {
+  description = "RDS credentials secret name"
+  value       = module.secretsmanager.secret_name
+}
+
+output "db_secret_arn" {
+  description = "RDS credentials secret ARN"
+  value       = module.secretsmanager.secret_arn
+}
