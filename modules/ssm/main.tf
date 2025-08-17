a5456f6987af72a57a05796b99d80007465d97e7
Jagadishhs69 <hsjagadish42@gmail.com>
2025-08-17 18:40:13 +0530

    removed app role name as it was duplicated

--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -1,8 +1,3 @@
-variable "app_role_name" {
-  description = "IAM role name of the EC2 instance"
-  type        = string
-}
-
 resource "aws_iam_role_policy_attachment" "ssm_managed" {
   role       = var.app_role_name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
