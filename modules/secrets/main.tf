e77041427f6ae70092d6fcc6e38ef4dca1f89915
Jagadishhs69 <hsjagadish42@gmail.com>
2025-08-17 19:04:54 +0530

    ssm

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -1,9 +1,9 @@
-resource "aws_secretsmanager_secret" "db_secret" {
+resource "aws_secretsmanager_secret" "rds_credentials" {
   name = "${var.env}-rds-credentials"
 }
 
-resource "aws_secretsmanager_secret_version" "db_secret_version" {
-  secret_id     = aws_secretsmanager_secret.db_secret.id
+resource "aws_secretsmanager_secret_version" "rds_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.rds_credentials.id
   secret_string = jsonencode({
     username = var.db_username
     password = var.db_password
--- modules/ssm/main.tf
+++ modules/ssm/main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role_policy_attachment" "ssm_managed" {
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance" {
   role       = var.app_role_name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
